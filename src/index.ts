2c1ca7f5b7da1341f2c5d8813fa29133329ce36d
Bobbie Soedirgo <bobbie@soedirgo.dev>
2023-07-27 19:27:10 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import { serve } from "https://deno.land/std@0.168.0/http/server.ts" // Cannot find module 'https://deno.land/std@0.168.0/http/server.ts' or its corresponding type declarations.ts(2307)
+
+Buffer
---
+++ supabase/functions/foo/index.ts
@@ -0,0 +1,3 @@
+import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
+
+Buffer // Cannot find name 'Buffer'.deno-ts(2580)
